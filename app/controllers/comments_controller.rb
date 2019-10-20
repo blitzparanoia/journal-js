85005f19524c95c0252d85fcddf1d186e5122d79
blitzparanoia <bluemilk44@gmail.com>
2019-10-20 16:44:45 -0400

    added route for comments, and link to show, added new method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,31 @@
 class CommentsController < ApplicationController
 
+  def new
+    @entry = Entry.find(params[:entry_id])
+    @comment = @entry.comments.build
+  end
+
   def create
+    @entry = Entry.find(params[:entry_id])
+    @comment = @entry.comments.build
+    if @comment.update(comment_params)
+      redirect_to entry_path(@entry)
   end
 
   def destroy
+    set_comment
+    @comment.delete
+    redirect_to comments_path
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :entry_id)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
 end
