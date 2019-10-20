79b20b8e737351601b2ca2320ebde28486269123
blitzparanoia <bluemilk44@gmail.com>
2019-10-20 18:17:13 -0400

    Entry and Comment models functioning

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,32 +1,58 @@
 class CommentsController < ApplicationController
 
-  def new
-    @entry = Entry.find(params[:entry_id])
-    @comment = @entry.comments.build
+  # def create
+  # 		@entry = Entry.find(params[:entry_id])
+  # 	 	@comment = @entry.comments.create(params[:comment].permit(:comment))
+  # 		redirect_to entry_path(@entry)
+  # 	end
+  #
+  # 	def destroy
+  # 		@entry = Entry.find(params[:entry_id])
+  # 		@comment = @entry.comments.find(params[:id])
+  # 		@comment.destroy
+  # 		redirect_to entry_path(@entry)
+  # 	end
+
+  def index
+    if params[:entry_id]
+			@entry = Entry.find(params[:entry_id])
+			@comments = Comment.all.where(entry_id: params[:entry_id])
+
+		else
+			@comments = Comment.all
+	end
   end
 
-  def create
-    @entry = Entry.find(params[:entry_id])
-    @comment = @entry.comments.build
-    if @comment.update(comment_params)
-      redirect_to entry_path(@entry)
+    def new
+      @entry = Entry.find(params[:entry_id])
+      @comment = @entry.comments.build
     end
-  end
 
-  def destroy
-    set_comment
-    @comment.delete
-    redirect_to comments_path
+    def create
+      @entry = Entry.find(params[:entry_id])
+      @comment = @entry.comments.build
+      if @comment.update(comment_params)
+         redirect_to entry_path(@entry)
+  else
+    render :new
   end
+end
 
-  private
+    def show
+      @comment = Comment.find(id: params[:id])
+    end
 
-  def comment_params
-    params.require(:comment).permit(:content, :entry_id)
-  end
 
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
+
+
+
+
+
+
+
+private
+		def comment_params
+			params.require(:comment).permit(:entry_id, :content)
+		end
 
 end
