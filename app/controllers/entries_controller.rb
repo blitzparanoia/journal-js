823572e6dd2183834959b88f0938e25e4ccef280
blitzparanoia <bluemilk44@gmail.com>
2019-10-20 14:25:01 -0400

    added form and methods layout to entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,10 +4,33 @@ class EntriesController < ApplicationController
   end
 
   def new
-    @entries = Entry.new
+    @entry = Entry.new
   end
 
   def create
+    @entry = Entry.new(entry_params)
+
+    if @entry.save
+      redirect_to @entry
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :content, :mood)
   end
 
 end
