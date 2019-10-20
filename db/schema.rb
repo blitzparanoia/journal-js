42e0cb1278d394b261567be80ef5c6efe4adf909
blitzparanoia <bluemilk44@gmail.com>
2019-10-20 16:26:58 -0400

    added column to table, building comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_181643) do
+ActiveRecord::Schema.define(version: 2019_10_20_201352) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "entry_id"
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string "title"
