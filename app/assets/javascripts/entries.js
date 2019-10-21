99474a588c52545ff4546e3ff9b87dbf1fc4e46e
blitzparanoia <bluemilk44@gmail.com>
2019-10-20 22:23:20 -0400

    constructor for entry class created

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -9,14 +9,31 @@ $(function(){
 function listenForClick() {
     $('#new-entry').click(function (event) {
       // alert('Yay');
-      event.preventDefault()
-      getEntries()
+      // event.preventDefault()
+      let newEntryForm = Entry.newEntryForm()
+      document.querySelector('div#entry-form-div').innerHTML = newEntryForm
     })
 }
-
-
-
+//
 //get req for data
+// function getEntries() {
+//     $.ajax({
+//       url: 'http://localhost:3000/entries'
+//     })
+// }
+
+
+
 
 
 // class constructor for form and format/rendering
+class Entry {
+  constructor(e) {
+    this.id = e.id
+    this.title = e.title
+    this.content = e.content
+    this.comments = e.comments
+  }
+
+  
+}
