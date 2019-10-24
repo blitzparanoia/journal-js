22bd9eedee373bcbdbb1016230395ca8b722eb13
blitzparanoia <bluemilk44@gmail.com>
2019-10-24 18:42:09 -0400

    fixed more content link not include # in link, combined comments click function and get request to append

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,53 +1,28 @@
 $(function () {
-  commentClick()
+  getComments()
 })
 
-function commentClick(){
-  $('button#comment-data').one('click', function(e) {
-    e.preventDefault()
-    getComments()
-  })
-}
+function getComments () {
+  $("button#comment-data").one("click", function(e) {
+   e.preventDefault();
+      let id = $(this).data("id");
+      $.get(`/entries/${id}/comments.json`, function(data){
+        let comments = data
+        for(i = 0; i< comments.length; i++){
+          $('#ajax-comments').append(`
 
-//uses AJAX to get the comment information, then attach info to the desired div named
-function getComments() {
-	$.ajax({
-		url: `http://localhost:3000/entries`,
-		method: 'get',
-		dataType: 'json',
-		success: function (data) {
-      // alert("hi")
-			data.map(comments => {
-				const newComment = new Comment(comments)
-				const newCommentHtml = newComment.postHTML()
-				document.getElementById('ajax-comments').innerHTML += newCommentHtml
-      })
-      		}
-      	})
-      }
+            <li> ${comments[i].content} </li>
+
+            `)
+        }
+      });
+  });
+};
+
 
 class Comment {
 	constructor(c) {
 		this.id = c.id
 		this.comments = c.comments
 	}
-}
-
-
-// Layout formating for the constructor
-  Comment.prototype.postHTML = function () {
-  // set a vaiable with the comment param that is captured from the JSON
-	let postComments = this.comments.map(comment => {
-		return (`
-			<li>${comment.content}</li>
-		`)
-	}).join('')
-
-	return (`
-		<div class='commentview'>
-    <ol>
-			${postComments}
-      </ol>
-		</div>
-	`)
 }
