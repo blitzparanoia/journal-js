94851e00050bb065ecfe8f47311672a994638891
blitzparanoia <bluemilk44@gmail.com>
2019-10-26 18:03:45 -0400

    Rebuilding entries js, moved entry pop up to app layout page, created a constructor class

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,16 +1,26 @@
+//
+// $(function() {
+//   $(".js-list").on("click", function(event) {
+//     event.preventDefault()
+//     let $list = $("#entryShow");
+//     var entryId = parseInt($(".js-list").attr("data-id"));
+//     $.get("/entries/" + entryId + ".json",
+//     function(data) {
+//       $list.htmlBlock('')
+//       data.entries(function(entry){
+//        displayEntry = new Entry(entry)
+//          displayEntry.htmlBlock()
+//       })
+//     });
+//   });
+// });
 
-$(function() {
-  $(".js-list").on("click", function(event) {
-    event.preventDefault()
-    let $list = $("#entryShow");
-    var entryId = parseInt($(".js-list").attr("data-id"));
-    $.get("/entries/" + entryId + ".json",
-    function(data) {
-      $list.htmlBlock('')
-      data.entries(function(entry){
-       displayEntry = new Entry(entry)
-         displayEntry.htmlBlock()
-      })
-    });
-  });
-});
+class Entry {
+	constructor(obj) {
+		this.id = obj.id
+		// this.created_at = obj.created_at
+		this.title = obj.title
+		this.content = obj.content
+		this.comments = obj.comments
+	}
+}
